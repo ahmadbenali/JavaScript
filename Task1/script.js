a1f1b7be5cd005c52964943f262b9c99b5b46c6b
ahmadbenali <a7madbenali@gmail.com>
2026-09-22 15:04:54 +0300

    New update on task1 & add task2

--- Task1/script.js
+++ Task1/script.js
@@ -1,3 +1,18 @@
+function showMenu(menu){
+    document.write("<h2>--- Menu ---</h2><ul>");
+    for(let food of menu){
+        document.write(`${food.name}<br>`);
+    }
+}
+function availableFood(foodName,menu){
+
+    for(let i in menu){
+        if(foodName===menu[i].name)
+            return true;
+    }
+    return false;
+}
+
 let userName=prompt("Enter your name: ");
 let userAge=prompt("Enter your Age: ");
 let userGender=prompt("Enter your Gender(Male/Female): ");
@@ -14,15 +29,18 @@ if (userGender !== "Male" && userGender !== "Female") {
 
 
 if(userGender==="Male"){
-    document.getElementById("info").innerHTML=`<h2>Welcome Mr. ${userName} </h2>`;
+    document.write(`<h2>Welcome Mr. ${userName} </h2>`);
+    //document.getElementById("info").innerHTML=`<h2>Welcome Mr. ${userName} </h2>`;
 }else if(userGender==="Female"){
-    document.getElementById("info").innerHTML=`<h2>Welcome Ms. ${userName} </h2>`;
-}else document.getElementById("info").innerHTML=`<h2>Welcome ${userName} </h2>`;
+    document.write(`<h2>Welcome Ms. ${userName} </h2>`);
+    //document.getElementById("info").innerHTML=`<h2>Welcome Ms. ${userName} </h2>`;
+}else document.write(`<h2>Welcome ${userName} </h2>`);
 
 let order;
 let vaild=true;
 if(userAge<16){
-    document.getElementById("eli").innerHTML=`<h2> You are not eligible to place an order </h2>`;
+    document.write(`<h2> You are not eligible to place an order </h2>`);
+    //document.getElementById("eli").innerHTML=`<h2> You are not eligible to place an order </h2>`;
 }else{
     order=prompt("Choose an Order: Burger/Shawarma/Zinger");
     if(order!=="Burger" && order!=="Shawarma" && order!=="Zinger"){
@@ -32,9 +50,11 @@ if(userAge<16){
 }
 
 if(userAge>18 && vaild){
-    document.getElementById("order").innerHTML=`<h2>Order Confirmed </h2>`;
+    document.write(`<h2>Order Confirmed </h2>`);
+    //document.getElementById("order").innerHTML=`<h2>Order Confirmed </h2>`;
 }else if(userAge<18 || vaild){
-    document.getElementById("order").innerHTML=`<h2>Order requires verification </h2>`;
+    document.write(`<h2>Order requires verification </h2>`);
+    //document.getElementById("order").innerHTML=`<h2>Order requires verification </h2>`;
 }
 
 let sta;
@@ -49,7 +69,35 @@ console.log(userName);
 console.log(userGender);
 console.log(userAge);
 
-document.getElementById("final").innerHTML=`<h2>Final order:<br>${userName} <br> ${userAge} <br> ${userGender} <br> ${order} <br> ${sta}</h2>`;
+document.write(`<h2>Final order:<br>${userName} <br> ${userAge} <br> ${userGender} <br> ${order} <br>`);
+//document.getElementById("final").innerHTML=`<h2>Final order:<br>${userName} <br> ${userAge} <br> ${userGender} <br> ${order} <br> ${sta}</h2>`;
+
+// =================================TASK 2=============================================================================
+let menu=[
+    {
+        "name":"Burger",
+        "price":20,
+        "category":"Fast Food",
+        "available":true
+    },{
+        "name":"Shawarma",
+        "price":15,
+        "category":"Fast Food",
+        "available":true
+    }
+]
+
+showMenu(menu);
+
+let foodName=prompt("Enter food name for the menu: ");
+
+while(!availableFood(foodName,menu)){
+    foodName=prompt("Renter food name for the menu: ");
+}
+
+
+
+
 
 
 
